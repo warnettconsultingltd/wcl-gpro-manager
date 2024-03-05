772ae4fb53681648105950c0c876a11566a5b12a
wcl-martinwarnett <coding@warnettconsultingltd.co.uk>
2024-03-05 13:33:39 +0000

    Deleted .java files

--- parsers/src/main/java/wcl/gpromanager/parsers/utils/FloatValueRetriever.java
+++
@@ -1,8 +0,0 @@
-package wcl.gpromanager.parsers.utils;
-
-public class FloatValueRetriever implements ValueRetriever<Float> {
-    @Override
-    public Float convert(String attributeValue) {
-        return Float.parseFloat(attributeValue);
-    }
-}
--- parsers/src/main/java/wcl/gpromanager/parsers/utils/StringValueRetriever.java
+++
@@ -1,8 +0,0 @@
-package wcl.gpromanager.parsers.utils;
-
-public class StringValueRetriever implements ValueRetriever<String> {
-    @Override
-    public String convert(String attributeValue) {
-        return attributeValue;
-    }
-}
