9db2558c801eeb61aa7de8320bf9a5209ae134a9
wcl-martinwarnett <coding@warnettconsultingltd.co.uk>
2024-03-05 14:01:57 +0000

    Deletion of Java class

--- parsers/src/main/java/wcl/gpromanager/parsers/utils/IntegerValueRetriever.java
+++
@@ -1,9 +0,0 @@
-package wcl.gpromanager.parsers.utils;
-
-
-public final class IntegerValueRetriever implements ValueRetriever<Integer> {
-    @Override
-    public Integer convert(String attributeValue) {
-        return Integer.parseInt(attributeValue);
-    }
-}
